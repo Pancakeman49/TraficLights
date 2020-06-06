a582360c8da4eb81e6de2f42e745207aad02f3ce
TheRealPancakeman <rolands.silins345@gmail.com>
2020-06-06 17:53:49 +0300

    Working lights V1

--- TraficLights.cs
+++ TraficLights.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,18 +15,23 @@ namespace Trafic_lights
 {
     public partial class TraficLights : Form
     {
-        private Timer timerSwitch;
-        int time = 0;
-        int redInterval = 10;
-        int yellowInterval = 3;
-        int greenInterval = 6;
-        int totalTime = 0;
+        private Timer timerSwitch = null;
+        private Timer timerBlink = null;
+        private int redInterval = 10;
+        private int yellowInterval = 3;
+        private int greenInterval = 6;
+        private int timer = 0;
+        private bool reverseLights = false;
+
+        
         public TraficLights()
         {
             InitializeComponent();
             InitilizeProgram();
             InitiulizeTimerSwitch();
-            ClaculateStuffIdkDontAskMeWhatItDoesIDontKnowAndIDontWantToKnowWaitIDoKnowWhatItDoesIsntThisMethodsNameReallyLong();
+            InitilizeTimerBlink();
+
+
         }
         private void InitilizeProgram()
         {
@@ -43,27 +50,65 @@ namespace Trafic_lights
             timerSwitch.Tick += new EventHandler(TimerSwitch_Tick);
             timerSwitch.Start();
         }
+        private void InitilizeTimerBlink()
+        {
+            timerBlink = new Timer();
+            timerBlink.Interval = 500;
+            timerBlink.Tick += new EventHandler(TimerBlink_Tick);
+            
+        }
+        private void TimerBlink_Tick(object sender, EventArgs e)
+        {
+            GreenLightBlink();
+        }
         private void TimerSwitch_Tick(object sender, EventArgs e)
         {
-            time++; 
-            if (time >= totalTime)
+            timer++;
+            if (reverseLights == false)
             {
-                time = 0;
+                if (timer < redInterval)
+                {
+                    RedLight.BackColor = Color.Red;
+                }
+                if (timer == redInterval)
+                {
+                    YellowLight.BackColor = Color.Yellow;
+                }
+                if (timer == redInterval + yellowInterval)
+                {
+                    RedLight.BackColor = Color.Gray;
+                    YellowLight.BackColor = Color.Gray;
+                    GreenLight.BackColor = Color.Green;
+                    timerBlink.Start();
+                    reverseLights = true;
+                }
             }
-            if (time < redInterval)
+            else
+            {
+                if (timer == redInterval + yellowInterval + greenInterval)
+                {
+                    timerBlink.Stop();
+                    GreenLight.BackColor = Color.Gray;
+                    YellowLight.BackColor = Color.Yellow;
+                }
+                if (timer == redInterval + yellowInterval + greenInterval + yellowInterval)
+                {
+                    YellowLight.BackColor = Color.Gray;
+                    RedLight.BackColor = Color.Red;
+                    reverseLights = false;
+                    timer = 0;
+                }
+            }
+        }
+        private void GreenLightBlink()
+        {
+            if (GreenLight.BackColor == Color.Gray)
+            {
+                GreenLight.BackColor = Color.Green;
+            }
+            else
             {
                 GreenLight.BackColor = Color.Gray;
-                RedLight.BackColor = Color.Red;
-            }
-            if (time == redInterval)
-            {
-                RedLight.BackColor = Color.Gray;
-                YellowLight.BackColor = Color.Yellow;
-            }
-            if (time - redInterval == yellowInterval)
-            {
-                YellowLight.BackColor = Color.Gray;
-                GreenLight.BackColor = Color.Green;
             }
         }
         private void Buttons_click(object sender, EventArgs e)
@@ -81,11 +126,46 @@ namespace Trafic_lights
             {
                 greenInterval = Decimal.ToInt32(GreenNumeric.Value);
             }
-            ClaculateStuffIdkDontAskMeWhatItDoesIDontKnowAndIDontWantToKnowWaitIDoKnowWhatItDoesIsntThisMethodsNameReallyLong();
         }
-        private void ClaculateStuffIdkDontAskMeWhatItDoesIDontKnowAndIDontWantToKnowWaitIDoKnowWhatItDoesIsntThisMethodsNameReallyLong()
+
+
+
+
+
+
+
+
+
+
+
+
+        private void SwitchLights()//code that replaces if else statements 
         {
-            totalTime = redInterval + yellowInterval + greenInterval;
+            int seconds = 0;
+            switch (seconds)
+            {
+                case 0:
+                    RedLight.BackColor = Color.Red;
+                    break;
+                case 3:
+                    YellowLight.BackColor = Color.Yellow;
+                    RedLight.BackColor = Color.Gray;
+                    break;
+                case 5:
+                    YellowLight.BackColor = Color.Gray;
+                    GreenLight.BackColor = Color.Green;
+                    break;
+                case 8:
+                    YellowLight.BackColor = Color.Yellow;
+                    GreenLight.BackColor = Color.Gray;
+                    break;
+                case 10:
+                    YellowLight.BackColor = Color.Gray;
+                    RedLight.BackColor = Color.Red;
+                    seconds = -1;
+                    break;
+            }
+            seconds++;
         }
     }
 }
